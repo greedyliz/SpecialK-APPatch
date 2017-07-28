faaaba77348b1a892951977b9dcfebb23a4887cc
greedyliz <15964012+greedyliz@users.noreply.github.com>
2017-07-28 20:27:14 +0100

    Replaced leftover username references

--- depends/build/Win32/CEGUI/Config.h
+++ depends/build/Win32/CEGUI/Config.h
@@ -194,7 +194,7 @@ setting CMake options and regenerating, rather than editing directly.
 // Define the default place where cegui will look for loadable modules
 // this can be changed at runtime via the CEGUI_MODULE_DIR environment var.
 #if defined(_WIN32) || defined(__WIN32__)
-#   define CEGUI_MODULE_DIR "C:/Users/Andon/Downloads/cegui-0.8.7/build/bin/"
+#   define CEGUI_MODULE_DIR "C:/Users/Persn/Downloads/cegui-0.8.7/build/bin/"
 #elif !defined(__ANDROID__)
 #   define CEGUI_MODULE_DIR "C:/Program Files (x86)/cegui/lib/cegui-0.8/"
 #endif
